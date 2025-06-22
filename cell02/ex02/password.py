fbeb4cd84988e97a7fb9c3a0d619fe607dd78002
bpoungku <bpoungku@student.42bangkok.com>
2025-06-22 08:02:55 +0000

    add password.py

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 def main():
-    # Store the correct password in a variable
     correct_password = "Python is awesome"
-    #Prompt the user to enter a password
     entered_password = input("Enter the password: ")
-
-    # Check if the entered password matches the correct one 
     if entered_password == correct_password: 
         print("ACCESS GRANTED")
     else:
